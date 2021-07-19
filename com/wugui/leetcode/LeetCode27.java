7cab19d746821a44b7bd6dd5a490a5839d034cf0
陈梓阳 <chenziyang@qinsilk.com>
2021-07-19 17:54:22 +0800

    LeetCode27：移除元素

---
+++ com/wugui/leetcode/LeetCode27.java
@@ -0,0 +1,45 @@
+package wugui.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 移除元素
+ *
+ * @author chenziyang
+ */
+public class LeetCode27 {
+
+    /*
+     * 给你一个数组 nums和一个值 val，你需要 原地 移除所有数值等于val的元素，并返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     *
+     * 提示： 0 <= nums.length <= 100
+     *        0 <= nums[i] <= 50
+     *        0 <= val <= 100
+     */
+
+    public static int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            if (nums[right] != val) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int length = removeElement(nums, 2);
+        System.out.println(length);
+        System.out.println(Arrays.toString(nums));
+    }
+
+
+}
